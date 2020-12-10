d38fc73195f470d34b3bc4d38ed1ce5c62b14661
aehoffer <aehoffer@gmail.com>
2020-12-10 01:19:34 -0500

    Added dynamic programming approach to part 2 along with naive approach for reference.

--- day9.rb
+++ day9.rb
@@ -15,7 +15,7 @@ end
 puts "#{ test_num }"
 
 # Part 2
-# Brute force for looking for contiguous sum of test number.. Is there a better way?
+# Naive brute force for looking for contiguous sum of test number in O(n^3) time.
 def contiguous_sum(candidates, target)
   (2...candidates.size).each do |len|
     candidates.each_cons(len) { |cons| return cons if cons.sum == target }
@@ -24,5 +24,28 @@ def contiguous_sum(candidates, target)
   nil
 end
 
-contiguous_numbers = contiguous_sum(NUMBERS[0...test_num_idx], test_num)
-puts "#{ contiguous_numbers.min + contiguous_numbers.max }"
+# Dynamic programming approach. Use all prefix sums of candidates(0...candidates.size)
+# to compute arbitrary contiguous size in O(n^2) time until we find the target.
+def contiguous_sum_smart(candidates, target)  
+  # Prefix sums (i, j) = (offset, size - 1) 
+  sums = []
+  offset, size = 0, 0
+  
+  catch :Done do
+    (0...candidates.size).each do |i|
+      sums << [candidates[i]]
+      (i + 1...candidates.size).each do |j| 
+        sums[i] << sums[i][j - i - 1] + candidates[j]
+        if sums[i][j - i] == target
+          offset, size = i, j - i + 1
+          throw :Done
+        end
+      end
+    end
+  end
+  
+  candidates[offset...offset + size]
+end
+
+contiguous_numbers = contiguous_sum_smart(NUMBERS[0...test_num_idx], test_num)
+puts "#{ contiguous_numbers.min + contiguous_numbers.max }" unless contiguous_numbers.empty?
