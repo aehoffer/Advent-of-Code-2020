7d34a7879ad9fd626a506b11737dfa021b26e77c
aehoffer <aehoffer@gmail.com>
2020-12-12 02:27:00 -0500

    Day 12 cleanup.

--- day12.rb
+++ day12.rb
@@ -13,13 +13,13 @@ def move_ship1(ship, instructions)
     when 'S'
       ship[:pos][:y] -= instr[1]
     when /[LR]/
-	  sign = sign = instr[0] == 'L' ? 1 : -1
+      sign = sign = instr[0] == 'L' ? 1 : -1
       ship[:degrees] = (ship[:degrees] + sign * instr[1]) % 360
     when 'F'
       case ship[:degrees]
-	  # Note: normally would do Math.cos/sin(degrees * 180 / Math.PI) for x / y normally,
-	  #       but easier this way since no need to deal with floats here, all
-	  #       angles are given as integral multiples of 180 degrees.
+      # Note: normally would do Math.cos/sin(degrees * 180 / Math.PI) for x / y normally,
+      #       but easier this way since no need to deal with floats here, All
+      #       angles are given as integral multiples of 180 degrees.
       when 0
         # E
         ship[:pos][:x] += instr[1]
@@ -56,27 +56,27 @@ def move_ship2(ship, waypoint, instructions)
     when 'S'
       waypoint[:pos][:y] -= instr[1]
     when /[LR]/
-	  # Rotate waypoint around the ship to do stuff.
-	  # Co-ordinates are given relative to ship, so just directly 
-	  # multiply with imaginary units or -1 as needed.
-	  sign = instr[0] == 'L' ? 1 : -1
-	  angle = (sign * instr[1]) % 360
-	  
-	  complex_dir = case angle
-	    when 90
-		  'i'.to_c
-		when 180
-		  '-1'.to_c
-		when 270
-		  '-i'.to_c
-	  end
-	  
-	  waypoint_c = Complex(waypoint[:pos][:x], waypoint[:pos][:y]) * complex_dir
-	  waypoint[:pos][:x] = waypoint_c.real
-	  waypoint[:pos][:y] = waypoint_c.imag
+      # Rotate waypoint around the ship to do stuff.
+      # Co-ordinates are given relative to ship, so just directly 
+      # multiply with imaginary units or -1 as required.
+      sign = instr[0] == 'L' ? 1 : -1
+      angle = (sign * instr[1]) % 360
+    
+      complex_dir = case angle
+      when 90
+        'i'.to_c
+      when 180
+        '-1'.to_c
+      when 270
+        '-i'.to_c
+      end
+    
+      waypoint_c = Complex(waypoint[:pos][:x], waypoint[:pos][:y]) * complex_dir
+      waypoint[:pos][:x] = waypoint_c.real
+      waypoint[:pos][:y] = waypoint_c.imag
     when 'F'
       ship[:pos][:x] += instr[1] * waypoint[:pos][:x]
-	  ship[:pos][:y] += instr[1] * waypoint[:pos][:y]
+      ship[:pos][:y] += instr[1] * waypoint[:pos][:y]
     end
     # puts "instr: #{instr}, ship: #{ship}, waypoint: #{waypoint}"
   end
