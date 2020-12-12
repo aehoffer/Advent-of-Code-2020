c3105126ad9fbac7069da142f0425e7248a16bf7
aehoffer <aehoffer@gmail.com>
2020-12-12 04:31:34 -0500

    Rewritten Day 12 to use complex numbers for both parts to generalize.

--- day12.rb
+++ day12.rb
@@ -1,89 +1,38 @@
 INSTRUCTIONS = File.readlines("day12_input.txt").map { |line| [line.chomp[0], line.chomp[1..].to_i] }
 
-# Part 1
-def move_ship1(ship, instructions)
+def move_ship(ship, instructions)
   instructions.each do |instr|
     case instr[0]
-    when 'E'
-      ship[:pos][:x] += instr[1]
-    when 'N'
-      ship[:pos][:y] += instr[1]
-    when 'W'
-      ship[:pos][:x] -= instr[1]
-    when 'S'
-      ship[:pos][:y] -= instr[1]
+    when /[ENWS]/
+	  pos_add = case instr[0]
+	  when 'E'
+	    Complex(instr[1], 0)
+	  when 'N'
+	    Complex(0, instr[1])
+	  when 'W'
+	    Complex(-instr[1], 0)
+	  when 'S'
+	    Complex(0, -instr[1])
+	  end
+	  
+	  ship[:pos] += pos_add
+	when 'F'
+      ship[:pos] += ship[:wp] * instr[1]
     when 'L', 'R'
-      sign = instr[0] == 'L' ? 1 : -1
-      ship[:degrees] = (ship[:degrees] + sign * instr[1]) % 360
-    when 'F'
-      case ship[:degrees]
-      # Note: normally would do Math.cos/sin(degrees * Math.PI / 180) for x / y normally,
-      #       but easier this way since no need to deal with floats here, as all
-      #       angles are given as integral multiples of 180 degrees.
-      when 0
-        # E
-        ship[:pos][:x] += instr[1]
-      when 90
-        # N
-        ship[:pos][:y] += instr[1]
-      when 180
-        # W
-        ship[:pos][:x] -= instr[1]
-      when 270
-        # S
-        ship[:pos][:y] -= instr[1]
-      end
-    end
-    #puts "instr: #{instr}, ship: #{ship}"
+	  sign = instr[0] == 'L' ? 1 : -1
+      degrees = (sign * instr[1]) % 360
+      wp_dir = Complex::polar(1, Math::PI * degrees / 180)
+	  
+	  ship[:wp] *= wp_dir 
   end
 end
 
-ship1 = { pos: { x: 0, y: 0 }, degrees: 0 }
-move_ship1(ship1, INSTRUCTIONS)
-
-puts "#{ ship1[:pos][:x].abs + ship1[:pos][:y].abs }"
+# Part 1
+ship1 = { pos: Complex(0, 0), wp: Complex(1, 0) }
+move_ship(ship1, INSTRUCTIONS)
+puts "#{ ship1[:pos].real.abs + ship1[:pos].imag.abs }"
 
 # Part 2
-def move_ship2(ship, waypoint, instructions)
-  instructions.each do |instr|
-    case instr[0]
-    when 'E'
-      waypoint[:pos][:x] += instr[1]
-    when 'N'
-      waypoint[:pos][:y] += instr[1]
-    when 'W'
-      waypoint[:pos][:x] -= instr[1]
-    when 'S'
-      waypoint[:pos][:y] -= instr[1]
-    when 'L', 'R'
-      # Rotate waypoint around the ship to do stuff.
-      # Co-ordinates are given relative to ship, so just directly 
-      # multiply with imaginary units or -1 as required.
-      sign = instr[0] == 'L' ? 1 : -1
-      angle = (sign * instr[1]) % 360
-    
-      complex_dir = case angle
-      when 90
-        'i'.to_c
-      when 180
-        '-1'.to_c
-      when 270
-        '-i'.to_c
-      end
-    
-      waypoint_c = Complex(waypoint[:pos][:x], waypoint[:pos][:y]) * complex_dir
-      waypoint[:pos][:x] = waypoint_c.real
-      waypoint[:pos][:y] = waypoint_c.imag
-    when 'F'
-      ship[:pos][:x] += instr[1] * waypoint[:pos][:x]
-      ship[:pos][:y] += instr[1] * waypoint[:pos][:y]
-    end
-    # puts "instr: #{instr}, ship: #{ship}, waypoint: #{waypoint}"
-  end
-end
-
-ship2 = { pos: { x: 0, y: 0 }, degrees: 0 }
-waypoint = { pos: { x: 10, y: 1 } }
-
-move_ship2(ship2, waypoint, INSTRUCTIONS)
-puts "#{ ship2[:pos][:x].abs + ship2[:pos][:y].abs }"
+ship2 = { pos: Complex(0, 0), wp: Complex(10, 1) }
+move_ship(ship2, INSTRUCTIONS)
+puts "#{ ship2[:pos].real.abs + ship2[:pos].imag.abs }"
