4038fde8609abd906682ab58380c11b25d6db3ae
aehoffer <aehoffer@gmail.com>
2020-12-19 07:11:55 -0500

    Still WIP for part 2 day 19, not sure at this point what to do.

--- day19.rb
+++ day19.rb
@@ -6,11 +6,11 @@ rule_tree = {}
 RULES.each do |r|
   matches = r.match(/(\d+): (.+)/)
 
-  parent = matches[1]
+  parent = matches[1].to_i
   rule_tree[parent] = []
 
   child_groups = matches[2].split('|').map { |g| g.strip.tr('"', '') }
-  child_groups.each { |g| rule_tree[parent] << g.split(' ').map { |c| c } }
+  child_groups.each { |g| rule_tree[parent] << g.split(' ').map { |c| c =~ /\d+/ ? c.to_i : c } }
 end
 
 # Part 1
@@ -19,7 +19,7 @@ def derive_messages(tree, start)
 
   derive_messages_rec = lambda do |rule|
     return cache[rule] unless cache[rule].nil?
-    return cache[rule] = [tree[rule].first.first] if tree[rule].first.first =~ /[ab]\*?/
+    return cache[rule] = [tree[rule].first.first] unless tree[rule].first.first.is_a?(Integer)
 
     messages = []
     # puts "Child rules: #{tree[rule]}"
@@ -35,12 +35,12 @@ def derive_messages(tree, start)
 
   derive_messages_rec.call(start)
 end
-possible_messages = derive_messages(rule_tree, '0').sort!
+possible_messages = derive_messages(rule_tree, 0).sort!
 puts MESSAGES.reject { |m| possible_messages.bsearch { |s| m <=> s }.nil? }.size
 
+# giant_regex = possible_messages.map { |m| /"#{m}"/ }.join('|')
+# puts MESSAGES.select { |m| giant_regex.match?(m) }.size
+
 # Part 2
-rule_tree['8'] = [['42', '8*']]
-rule_tree['11'] = [['42', '11*', '31']]
-
-# ??? What do?
-
+rule_42 = derive_messages(rule_tree, 42).map { |m| /"#{m}"/ }.join('|')
+rule_31 = derive_messages(rule_tree, 31).map { |m| /"#{m}"/ }.join('|')
